b59b173af2832f4b25e298b6788bfd7e7c8116a0
poenaru mihai <poenarumihai2008@gmail.com>
2023-05-13 15:36:01 +0300

    add Repository and DataAccess

---
+++ CoffeeShop_WebAPI/WebApplication1/DataAccess/CoffeeShopContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Model;
+
+namespace WebApplication1.DataAccess
+{
+    public class CoffeeShopContext :DbContext
+    {
+        public CoffeeShopContext(DbContextOptions<CoffeeShopContext> options) : base(options) { }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ CoffeeShop_WebAPI/WebApplication1/DataAccess/Repository/CoffeeShopUsersRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Model;
+
+namespace WebApplication1.DataAccess.Repository
+{
+    public class CoffeeShopUsersRepository : ICoffeeShopRepository<Users>
+    {
+        private readonly CoffeeShopContext _context;
+        public CoffeeShopUsersRepository(CoffeeShopContext context) 
+        {
+            _context = context; 
+        }
+
+        public async Task<IEnumerable<Users>> GetAll()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public Task<Users> Insert(Users item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Users> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Users item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
