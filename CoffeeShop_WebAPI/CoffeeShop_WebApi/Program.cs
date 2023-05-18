39b17683d2d464242ab77c3ad45b20fea47204d8
poenaru mihai <poenarumihai2008@gmail.com>
2023-05-19 02:07:32 +0300

    allow use swagger

--- CoffeeShop_WebAPI/CoffeeShop_WebApi/Program.cs
+++ CoffeeShop_WebAPI/CoffeeShop_WebApi/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 {
@@ -52,11 +52,11 @@ var app = builder.Build();
     app.MapControllers();
 }
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseCors("NgOrigins");
 
