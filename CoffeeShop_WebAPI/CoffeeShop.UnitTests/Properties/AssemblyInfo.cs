2c9a690f3c19db98393d520244cd03a2facb01ef
poenaru mihai <poenarumihai2008@gmail.com>
2023-05-27 16:58:02 +0300

    change the name for unit Tests project

--- CoffeeShop_WebAPI/CoffeeShop.UnitTests/Properties/AssemblyInfo.cs
+++ CoffeeShop_WebAPI/CoffeeShop.UnitTests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CoffeeShop_WebApi_UnitTests")]
+[assembly: AssemblyTitle("CoffeeShop.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoffeeShop_WebApi_UnitTests")]
+[assembly: AssemblyProduct("CoffeeShop.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("64fa458c-a140-4657-8e8b-c2be4cd508a9")]
+[assembly: Guid("fe9f8b56-a91d-48f1-9b0c-bcf71023d343")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- CoffeeShop_WebAPI/CoffeeShop.UnitTests/RegisterTests/UnitTest1.cs
+++ CoffeeShop_WebAPI/CoffeeShop.UnitTests/RegisterTests/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace CoffeeShop_WebApi_UnitTests
+namespace CoffeeShop.UnitTests
 {
     [TestClass]
     public class UnitTest1
