2596d2f52187afdbb8fb51e30ceb3ea323b3936c
Mihai Poenaru <poenarumihai2008@gmail.com>
2023-09-18 19:33:21 +0300

    navbar adaptive

--- CoffeeShopUI_Angular/src/app/app.module.ts
+++ CoffeeShopUI_Angular/src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ImgPresentationComponent } from './img-presentation/img-presentation.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ImgPresentationComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
