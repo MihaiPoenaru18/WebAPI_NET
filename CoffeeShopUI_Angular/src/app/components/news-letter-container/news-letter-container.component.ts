6906ec2f70fc91a6ae3ce9cef9e8346453906766
Mihai Poenaru <poenarumihai2008@gmail.com>
2023-10-03 15:13:31 +0300

    finish the send email logic

--- CoffeeShopUI_Angular/src/app/components/news-letter-container/news-letter-container.component.ts
+++ CoffeeShopUI_Angular/src/app/components/news-letter-container/news-letter-container.component.ts
@@ -1,31 +1,64 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup , FormBuilder, Validators} from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'news-letter-container',
   templateUrl: './news-letter-container.component.html',
-  styleUrls: ['./news-letter-container.component.css']
+  styleUrls: ['./news-letter-container.component.css'],
 })
 export class NewsLetterContainerComponent {
-  newsLetterForm = new FormGroup({
-    fullName : new FormControl(''),
-    email : new FormControl('')
+  constructor(private fb: FormBuilder) {}
+
+  newsLetterForm = this.fb.group({
+    fullName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
   });
-  submitted = false;
-   constructor(private fromBuilder: FormBuilder){ }
-   
-   ngOnInit(){
-    //validations
-    this.newsLetterForm = this.fromBuilder.group({
-      fullName:['',Validators.required],
-      email :['',Validators.required]
-    })
-   }
-   Onsubmit(){
-    this.submitted = true;
-    if(this.newsLetterForm.invalid){
-      return
+
+  isDisabled = false;
+  isSubmitted = true;
+  onSubmit(): void {
+    console.log(
+      'submitted form',
+      this.newsLetterForm.value,
+      this.newsLetterForm.valid
+    );
+    this.isSubmitted = true;
+    this.newsLetterForm.reset();
+  }
+  onUserInput(event: any) {
+    let inputText = event.target.value;
+    if (inputText == '') {
+      this.isSubmitted = true; // Make button disabled
+    } else {
+      this.isSubmitted = false; // Make button enabled
     }
-    alert("Success")
-   }
+  }
+  MessagePlaceholderFullName(): string {
+    if (
+      this.newsLetterForm.get('fullName')?.invalid &&
+      (this.newsLetterForm.get('fullName')?.dirty ||
+        this.newsLetterForm.get('fullName')?.touched)
+    ) {
+      return ' Required';
+    } else {
+      return ' Full Name';
+    }
+  }
+  MessagePlaceholderEmail(): string {
+    if (
+      this.newsLetterForm.get('email')?.invalid &&
+      (this.newsLetterForm.get('email')?.dirty ||
+        this.newsLetterForm.get('fullName')?.touched)
+    ) {
+      return 'Email Required';
+    } else {
+      return 'Email';
+    }
+  }
+ 
 }
