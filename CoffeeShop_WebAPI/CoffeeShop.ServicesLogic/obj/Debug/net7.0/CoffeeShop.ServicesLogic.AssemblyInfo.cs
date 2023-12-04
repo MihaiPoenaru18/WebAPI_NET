3659a8ff6b1d8cbd3cbd58e6c3ff8fcb976c1601
Mihai Poenaru <poenarumihai2008@gmail.com>
2023-12-04 20:26:38 +0200

    updare gitignor

--- CoffeeShop_WebAPI/CoffeeShop.ServicesLogic/obj/Debug/net7.0/CoffeeShop.ServicesLogic.AssemblyInfo.cs
+++ CoffeeShop_WebAPI/CoffeeShop.ServicesLogic/obj/Debug/net7.0/CoffeeShop.ServicesLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeShop.ServicesLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ac765b7f97c3f0ea3e2309e45e68cc8d164d29e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeShop.ServicesLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeShop.ServicesLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
