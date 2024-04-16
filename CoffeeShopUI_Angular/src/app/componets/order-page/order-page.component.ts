2b00e3b57ae0b8b98d5901621177639afd4bf249
Mihai Poenaru <poenarumihai2008@gmail.com>
2024-04-16 18:07:33 +0300

    add html and css to order page

--- CoffeeShopUI_Angular/src/app/componets/order-page/order-page.component.ts
+++ CoffeeShopUI_Angular/src/app/componets/order-page/order-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/Product/cart.service';
+import { ProductInterfaces } from '../Product-Page/product.interfaces';
 
 @Component({
   selector: 'cs-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.css'],
 })
-export class OrderPageComponent {
+export class OrderPageComponent implements OnInit {
+  @Input() cartProductsFromOrderpage: ProductInterfaces[];
+  totalPrice: number = 0;
+  constructor(private cartService: CartService) {}
+  ngOnInit() {
+    this.getProducts();
+    this.totalPriceProduct();
+  }
+  getProducts() {
+    this.cartService
+      .getProducts()
+      .subscribe((products) => (this.cartProductsFromOrderpage = products));
+  }
 
+  totalPriceProduct() {
+    this.cartService.totalPrices$.subscribe((total) => {
+      this.totalPrice = total;
+    });
+  }
+  removeProduct(product: ProductInterfaces) {
+    this.cartService.removeFromCart(product);
+  }
 }
